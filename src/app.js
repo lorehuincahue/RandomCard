946e6a7c209e95464e9bdfff0a8788718ec3474c
lorehuincahue <lorehuincahue@gmail.com>
2022-10-31 01:18:20 +0000

    avance

--- src/app.js
+++ src/app.js
@@ -2,10 +2,32 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector(".card").classList.add(generateRandomPintas());
+  document.querySelector(".card").innerHTML = generateRandomNumber();
+};
+let generateRandomNumber = function() {
+  let numeros = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  let indexNum = Math.floor(Math.random() * numeros.length);
+  return numeros[indexNum];
+};
+
+let generateRandomPintas = function() {
+  let pintascards = ["♥", "♠", "♣", "♦️"];
+  let indexPinta = Math.floor(Math.random() * pintascards.length);
+  return pintascards[indexPinta];
 };
